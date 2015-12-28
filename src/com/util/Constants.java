4b2388f934c216f39a61138e3bbef39424f7c584
saiinfra <skrishna@infrascape.com>
2015-12-28 15:31:23 +0530

    Update Constants.java

--- src/com/util/Constants.java
+++ src/com/util/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final String PASSWORD = "infrascape4YKyRvo9lxSLmmr20xAQ72zhI";
 	public static final String INSTANCE_URL = "https://na34.salesforce.com";
 	public static final String TestInformationID="a0361000005ZnOy";
-	public static final String TestScript="a0461000002hQlJAAU";
+	public static final String TestScript="a0461000002iJnq";
 	public static final String COMPLETED_STATUS="Completed";
 	
 
