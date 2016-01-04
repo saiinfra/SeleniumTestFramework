19618cd58fd4f9e609316e7add67b1a8383477ab
saiinfra <skrishna@infrascape.com>
2016-01-04 20:52:35 +0530

    Update Constants.java

--- src/com/salesforce/util/Constants.java
+++ src/com/salesforce/util/Constants.java
@@ -20,6 +20,7 @@ public class Constants {
 	public static String PackagePath = "com.test.";
 	public static String GitTestProjectURL="https://github.com/saiinfra/CustomerTestProject.git";
 	public static String CheckoutPath1 = AppUtil.getCurrentPath() + Constants.DirSeperator + Constants.CheckoutFolder;
+        public static String JavaSrcSearchPath = AppUtil.getCurrentPath() + Constants.DirSeperator + JavaSourcePath;
 
 	public static String CheckoutPath = AppUtil.getCurrentPath() + Constants.DirSeperator + Constants.CheckoutFolder+ Constants.DirSeperator+MappingFolderName;
 	public static String CheckoutFilePath = AppUtil.getCurrentPath() + Constants.DirSeperator + Constants.CheckoutFolder;
